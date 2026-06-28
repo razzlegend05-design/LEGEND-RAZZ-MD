bea1f7aeffe792d4a57f00b365915bd76c95b1e3
razzlegend05-design <razzlegend05@gmail.com>
2026-06-28 20:22:11 +0500

    Initial upload of LEGEND RAZZ MD
    Added all required source files for LEGEND RAZZ MD project.
    Ready for deployment on Render.

---
+++ settings.js
@@ -0,0 +1,6 @@
+module.exports = {
+    giphyApiKey: process.env.GIPHY_API_KEY || 'dc6zaTOxFJmzC',
+    ownerNumber: process.env.OWNER_NUMBER || '923000000000',
+    botName: '𝗟𝗘𝗚𝗘𝗡𝗗-𝗥𝗔𝗭𝗭 𝗠𝗗',
+    ownerName: '𝐋𝐄𝐆𝐄𝐍𝐃-𝐑𝐀𝐙𝐙'
+};
